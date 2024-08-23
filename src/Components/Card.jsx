4d02a00d664ef6a28d159bfda0e495707a215bd2
MOHD AQIB <mohdaqib921@gmail.com>
2024-08-23 17:33:34 +0530

    second

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,19 +1,20 @@
 import { Plus } from "lucide-react";
 import React from "react";
-import { Button,Card, Checkbox, Label, Modal, Textarea, TextInput } from "flowbite-react";
-import { useState } from "react";
+import { Button, Card } from "flowbite-react";
+import { truncate } from "lodash";
 
-const Cards = ({widget}) => {
-
+const Cards = ({ widget }) => {
   return (
-    
     <Card className="grid-cols-1 h-[15rem] bg-white flex justify-center items-start rounded-2xl">
       <div className=" flex w-full h-full flex-col gap-4">
         <h2 className="font-bold text-3xl text-zinc-900">{widget.name}</h2>
-        <p className="text-gray-700 font-medium">{widget?.information!=""?widget?.information:"No Information Available"}</p>
-      </div>
+        <p className="text-gray-700 font-medium">
+          {widget?.information != ""
+            ? truncate(widget?.information, { length: 160 })
+            : "No Information Available"}
+        </p>
+      </div>  
     </Card>
-
   );
 };
 
